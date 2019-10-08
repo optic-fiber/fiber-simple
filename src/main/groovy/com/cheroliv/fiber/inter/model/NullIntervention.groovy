9733cd596d70ffded25730348b712b4fcfc774aa
cheroliv <cheroliv@laposte.net>
2019-10-08 05:18:53 +0200

    ______

---
+++ src/main/groovy/com/cheroliv/fiber/inter/model/NullIntervention.groovy
@@ -0,0 +1,18 @@
+package com.cheroliv.fiber.inter.model
+
+import com.cheroliv.fiber.inter.domain.enumeration.ContractEnum
+import com.cheroliv.fiber.inter.domain.enumeration.TypeInterEnum
+
+import java.time.LocalDateTime
+
+class NullIntervention {
+    String getNd(){''}
+
+    TypeInterEnum getTypeInter(){null}
+
+    ContractEnum getContract(){null}
+
+    LocalDateTime getDateTimeInter(){null}
+    String getFirstNameClient(){''}
+    String getLastNameClient(){''}
+}
